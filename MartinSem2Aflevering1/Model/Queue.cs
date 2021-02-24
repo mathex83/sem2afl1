3d286f69623b5a4f51f01d5e74a3eca2ccdc8489
mathex83 <marnor83@gmail.com>
2021-02-24 09:52:12 +0100

    Add project files.

---
+++ MartinSem2Aflevering1/Model/Queue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace MartinSem2Aflevering1.Model
+{
+	public class Queue<T> : IEnumerable<T>, IEnumerable, IReadOnlyCollection<T>, ICollection
+	{
+		public Queue();
+		public Queue(IEnumerable<T> collection);
+	}
+}
